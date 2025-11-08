a6d58a2e11c300e74babb5a01193ef44f4dd572a
franmdelgado <fdelgadomartin1@gmail.com>
2025-11-08 13:34:22 -0300

    Se eliminan comentarios innecesarios

--- compras/src/main/java/com/taller/compras/entity/Producto.java
+++ compras/src/main/java/com/taller/compras/entity/Producto.java
@@ -27,7 +27,6 @@ public class Producto {
         this.precio = precio;
     }
 
-    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
--- compras/src/main/java/com/taller/compras/repository/ProductoRepositorio.java
+++ compras/src/main/java/com/taller/compras/repository/ProductoRepositorio.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-    // JpaRepository ya nos da todos los métodos CRUD básicos
+
 }
